b4f3b940d32e5398dc40ea1bae88cbe181f8b139
tangstadk <39906787+tangstadk@users.noreply.github.com>
2020-08-21 21:35:28 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -24,13 +24,13 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
+  /*
   $contact->smtp = array(
     'host' => 'email-smtp.eu-west-2.amazonaws.com',
     'username' => 'AKIASGMBBW7EHKADJIJ5',
     'password' => 'BHief2pL12Hzz6jneDv0xPtEemSKcUMmPZGWIKLT6uZG',
     'port' => '587'
-  );
+  );*/
   
 
   $contact->add_message( $_POST['name'], 'From');
